f3318ea42f43ad48faa80c4983f6f60b423c31c8
Nicole Sullivan <nicole@stubbornella.org>
2023-12-30 22:07:25 -0800

    Updates test to  text as designed, not currently achieved

--- tests-playwright/call.spec.ts
+++ tests-playwright/call.spec.ts
@@ -6,11 +6,11 @@ test('should navigate to caller game and test for title', async ({ page }) => {
     expect(altText).toBe('Climb Assist');
 });
 
-test('should display the call L when button clicked', async ({ page }) => {
+test('should display the call "L" when left hand clicked', async ({ page }) => {
     await page.goto('http://localhost:3000/call');
     await page.click('button:text("L")');
     let callText = await page.textContent('div.call'); 
-    expect(callText).toContain('Left');
+    expect(callText).toContain('L');
 });
 
   
